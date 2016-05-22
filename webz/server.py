a0d02c0a9c765325cf614879ded257b739b09453
Wouter <tuvokki@gmail.com>
2016-05-22 21:13:54 +0000

    keep one record of active pins

--- webz/server.py
+++ webz/server.py
@@ -6,6 +6,8 @@ import RPi.GPIO as GPIO
 
 app = Flask(__name__)
 
+activeleds = [12,24]
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
@@ -16,19 +18,20 @@ def togglepin(pin):
     GPIO.output(pin, GPIO.HIGH)
 
 def getstatus():
-  # return [{'led': 12, 'status': GPIO.input(12)}, {'led': 24, 'status': GPIO.input(24)}]
-  return [{12: GPIO.input(12), 24: GPIO.input(24)}]
+  status = {}
+  for led in activeleds:
+    GPIO.setup(led, GPIO.OUT)
+    status[led] = GPIO.input(led)
+  return [status]
 
 @app.route("/")
 def hello():
-  GPIO.setup(12, GPIO.OUT)
-  GPIO.setup(24, GPIO.OUT)
   ledstatus = getstatus()
   return render_template('hello.html', ledstatus=ledstatus)
 
 @app.route("/toggle/<int:pin_id>")
 def toggle(pin_id):
-  if pin_id == 12 or pin_id == 24:
+  if pin_id in activeleds:
     GPIO.setup(pin_id, GPIO.OUT)
     togglepin(pin_id)
     pin_status = GPIO.input(pin_id)
