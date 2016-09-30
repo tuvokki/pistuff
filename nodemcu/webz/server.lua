ba62c8cc315beb8e9075b34397acb763c3d34950
Wouter Roosendaal <tuvokki@gmail.com>
2016-09-30 08:04:33 +0200

    fixed wrong avrable assignments for colors

--- nodemcu/webz/server.lua
+++ nodemcu/webz/server.lua
@@ -1,8 +1,8 @@
-redled = 1
-whiteled = 2
+whiteled = 1
+redled = 2
 
-gpio.mode(redled, gpio.OUTPUT)
 gpio.mode(whiteled, gpio.OUTPUT)
+gpio.mode(redled, gpio.OUTPUT)
 
 srv=net.createServer(net.TCP)
 srv:listen(80,function(conn)
@@ -18,18 +18,20 @@ srv:listen(80,function(conn)
                 _GET[k] = v
             end
         end
-        buf = buf.."<h1>ESP8266 Web Server</h1>";
-        buf = buf.."<p>White led <a href=\"?pin=ONRED\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFFRED\"><button>OFF</button></a></p>";
-        buf = buf.."<p>Red led <a href=\"?pin=ONWHITE\"><button>ON</button></a>&nbsp;<a href=\"?pin=OFFWHITE\"><button>OFF</button></a></p>";
+        buf = buf.."<h1>ESP8266 light switch</h1>";
+        buf = buf.."<p>White led <a href=\"?pin=ONWHITE\"><button>ON</button></a>&nbsp;";
+        buf = buf.."<a href=\"?pin=OFFWHITE\"><button>OFF</button></a></p>";
+        buf = buf.."<p>Red led <a href=\"?pin=ONRED\"><button>ON</button></a>&nbsp;";
+        buf = buf.."<a href=\"?pin=OFFRED\"><button>OFF</button></a></p>";
         local _on,_off = "",""
-        if(_GET.pin == "ONRED")then
-              gpio.write(redled, gpio.HIGH);
-        elseif(_GET.pin == "OFFRED")then
-              gpio.write(redled, gpio.LOW);
-        elseif(_GET.pin == "ONWHITE")then
+        if(_GET.pin == "ONWHITE")then
               gpio.write(whiteled, gpio.HIGH);
         elseif(_GET.pin == "OFFWHITE")then
               gpio.write(whiteled, gpio.LOW);
+        elseif(_GET.pin == "ONRED")then
+              gpio.write(redled, gpio.HIGH);
+        elseif(_GET.pin == "OFFRED")then
+              gpio.write(redled, gpio.LOW);
         end
         client:send(buf);
         client:close();
