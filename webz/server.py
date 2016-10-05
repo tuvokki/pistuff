218e189b95642bb906e577a532052c641049c24f
Wouter <tuvokki@gmail.com>
2016-10-05 09:04:58 +0000

    Webz stuff

--- webz/server.py
+++ webz/server.py
@@ -3,10 +3,12 @@ from flask import Flask
 from flask import render_template
 import json
 import RPi.GPIO as GPIO
+from time import sleep
+from multiprocessing.pool import ThreadPool
 
 app = Flask(__name__)
 
-activeleds = [12,24]
+activeleds = [12,18,24,25]
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -24,10 +26,32 @@ def getstatus():
     status[led] = GPIO.input(led)
   return [status]
 
+def walk(wait, times):
+  for _ in range(times):
+    for led in activeleds:
+      GPIO.setup(led, GPIO.OUT)
+      togglepin(led)
+      sleep(wait/100.0)
+
 @app.route("/")
 def hello():
   ledstatus = getstatus()
   return render_template('hello.html', ledstatus=ledstatus)
+
+@app.route("/walker/<int:waittime>/<int:number>")
+def walker(waittime, number):
+  pool = ThreadPool(processes=1)
+  pool.apply_async(walk, (waittime, number))
+  return "done"
+
+@app.route("/status/")
+@app.route("/status/<int:pin_id>")
+def status(pin_id=None):
+  if not pin_id:
+    return getstatus()
+  else:
+    GPIO.setup(pin_id, GPIO.OUT)
+    return "status pin %s: %s" % (pin_id, GPIO.input(pin_id))
 
 @app.route("/toggle/<int:pin_id>")
 def toggle(pin_id):
