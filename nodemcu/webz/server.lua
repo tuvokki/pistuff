c47d50e2b74f7cdd3327a8393f293a8258becad5
Wouter Roosendaal <tuvokki@gmail.com>
2016-12-04 22:37:27 +0100

    Changed the parameters in the webinterface

--- nodemcu/webz/server.lua
+++ nodemcu/webz/server.lua
@@ -1,8 +1,4 @@
-whiteled = 1
-redled = 2
-
-gpio.mode(whiteled, gpio.OUTPUT)
-gpio.mode(redled, gpio.OUTPUT)
+powerpin = 1
 
 srv=net.createServer(net.TCP)
 srv:listen(80,function(conn)
@@ -18,20 +14,37 @@ srv:listen(80,function(conn)
                 _GET[k] = v
             end
         end
-        buf = buf.."<h1>ESP8266 light switch</h1>";
-        buf = buf.."<p>White led <a href=\"?pin=ONWHITE\"><button>ON</button></a>&nbsp;";
-        buf = buf.."<a href=\"?pin=OFFWHITE\"><button>OFF</button></a></p>";
-        buf = buf.."<p>Red led <a href=\"?pin=ONRED\"><button>ON</button></a>&nbsp;";
-        buf = buf.."<a href=\"?pin=OFFRED\"><button>OFF</button></a></p>";
+        buf = buf.."<!DOCTYPE html>";
+        buf = buf.."<html lang=\"en\">";
+        buf = buf.."<head>";
+        buf = buf.."<meta charset=\"utf-8\">";
+        buf = buf.."<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">";
+        buf = buf.."<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
+        buf = buf.."<title>Button</title>";
+        buf = buf.."<link href=\"http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css\" rel=\"stylesheet\">";
+        buf = buf.."</head>";
+        buf = buf.."<body id=\"page-top\">";
+        buf = buf.."<div class=\"jumbotron\">";
+        buf = buf.."<h1>This is a switch</h1>";
+        buf = buf.."<p>Just a simple on off switch</p>";
+        buf = buf.."<div class=\"btn-group\" role=\"group\" aria-label=\"switch\">";
+        -- read value of gpio 0.
+        if (gpio.read(powerpin) == gpio.LOW)then
+            buf = buf.."<a href=\"?pin=POWERON\" class=\"btn btn-default btn-xl\">ON</a>";
+            buf = buf.."<a href=\"?pin=POWEROFF\" class=\"btn btn-danger btn-xl\">OFF</a>";
+        else
+            buf = buf.."<a href=\"?pin=POWERON\" class=\"btn btn-primary btn-xl\">ON</a>";
+            buf = buf.."<a href=\"?pin=POWEROFF\" class=\"btn btn-default btn-xl\">OFF</a>";
+        end
+        buf = buf.."</div>";
+        buf = buf.."</div>";
+        buf = buf.."</body>";
+        buf = buf.."</html>";
         local _on,_off = "",""
-        if(_GET.pin == "ONWHITE")then
-              gpio.write(whiteled, gpio.HIGH);
-        elseif(_GET.pin == "OFFWHITE")then
-              gpio.write(whiteled, gpio.LOW);
-        elseif(_GET.pin == "ONRED")then
-              gpio.write(redled, gpio.HIGH);
-        elseif(_GET.pin == "OFFRED")then
-              gpio.write(redled, gpio.LOW);
+        if (_GET.pin == "POWEROFF")then
+            gpio.write(powerpin, gpio.HIGH);
+        elseif (_GET.pin == "POWERON")then
+            gpio.write(powerpin, gpio.LOW);
         end
         client:send(buf);
         client:close();
