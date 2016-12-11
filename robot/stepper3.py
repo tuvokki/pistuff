90175d00b7058288797a43b0558f06a530ebd25f
Wouter Roosendaal <tuvokki@gmail.com>
2016-12-11 22:33:35 +0100

    Linting on steppers

--- robot/stepper3.py
+++ robot/stepper3.py
@@ -11,67 +11,58 @@ GPIO.setmode(GPIO.BCM)
 # Define GPIO signals to use
 # Physical pins 11,15,16,18
 # GPIO17,GPIO22,GPIO23,GPIO24
-
-#StepPins = [17,22,23,24]
-StepPins = [19,20,21,26]
-
-# Set all pins as output
-for pin in StepPins:
-  print "Setup pins"
-  GPIO.setup(pin,GPIO.OUT)
-  GPIO.output(pin, False)
-
+STEP_PINS = [19, 20, 21, 26]
 # Define advanced sequence
 # as shown in manufacturers datasheet
-Seq = [[1,0,0,1],
-       [1,0,0,0],
-       [1,1,0,0],
-       [0,1,0,0],
-       [0,1,1,0],
-       [0,0,1,0],
-       [0,0,1,1],
-       [0,0,0,1]]
+SEQ = [[1, 0, 0, 1],
+       [1, 0, 0, 0],
+       [1, 1, 0, 0],
+       [0, 1, 0, 0],
+       [0, 1, 1, 0],
+       [0, 0, 1, 0],
+       [0, 0, 1, 1],
+       [0, 0, 0, 1]]
 
-StepCount = len(Seq)
-StepDir = 1 # Set to 1 or 2 for clockwise
-            # Set to -1 or -2 for anti-clockwise
+STEP_COUNT = len(SEQ)
+STEP_DIR = 1  # Set to 1 or 2 for clockwise
+# Set to -1 or -2 for anti-clockwise
 
 # Read wait time from command line
-if len(sys.argv)>1:
-  WaitTime = int(sys.argv[1])/float(1000)
+if len(sys.argv) > 1:
+    WAIT_TIME = int(sys.argv[1]) / float(1000)
 else:
-  WaitTime = 10/float(1000)
+    WAIT_TIME = 10 / float(1000)
 
 # Initialise variables
-StepCounter = 0
+STEP_COUNTER = 0
 
 # Start main loop
 try:
-  while True:
+    while True:
 
-    print StepCounter,
-    print Seq[StepCounter]
+        print STEP_COUNTER,
+        print SEQ[STEP_COUNTER]
 
-    for pin in range(0,4):
-      xpin=StepPins[pin]# Get GPIO
-      if Seq[StepCounter][pin]!=0:
-        print " Enable GPIO %i" %(xpin)
-        GPIO.output(xpin, True)
-      else:
-        GPIO.output(xpin, False)
+        for pin in range(0, 4):
+            xpin = STEP_PINS[pin]  # Get GPIO
+            if SEQ[STEP_COUNTER][pin] != 0:
+                print " Enable GPIO %i" % (xpin)
+                GPIO.output(xpin, True)
+            else:
+                GPIO.output(xpin, False)
 
-    StepCounter += StepDir
+        STEP_COUNTER += STEP_DIR
 
-    # If we reach the end of the sequence
-    # start again
-    if (StepCounter>=StepCount):
-      StepCounter = 0
-    if (StepCounter<0):
-      StepCounter = StepCount+StepDir
+        # If we reach the end of the sequence
+        # start again
+        if STEP_COUNTER >= STEP_COUNT:
+            STEP_COUNTER = 0
+        if STEP_COUNTER < 0:
+            STEP_COUNTER = STEP_COUNT + STEP_DIR
 
-    # Wait before moving on
-    time.sleep(WaitTime)
+        # Wait before moving on
+        time.sleep(WAIT_TIME)
 
 except KeyboardInterrupt:
-  # GPIO netjes afsluiten
-  GPIO.cleanup()
+    # GPIO netjes afsluiten
+    GPIO.cleanup()
