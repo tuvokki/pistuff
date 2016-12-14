b5016b03bee89cada497cb3830371a4ee1767983
Wouter <tuvokki@gmail.com>
2016-12-14 22:33:49 +0100

    Added some sequesnces and a turn

---
+++ robot/movement.sh
@@ -0,0 +1,8 @@
+sudo ./threaded.py --seconds 5 --direction -1
+sudo ./turn.py --seconds 2 --direction -1
+sudo ./threaded.py --seconds 5 --direction 1
+sudo ./turn.py --seconds 4 --direction 1
+sudo ./turn.py --seconds 4 --direction 1
+sudo ./threaded.py --seconds 5 --direction 1
+sudo ./turn.py --seconds 4 --direction 1
+sudo ./threaded.py --seconds 2 --direction 1
