812d615cbade8416cc9ae241d64100d761b2d4e2
Wouter Roosendaal <tuvokki@gmail.com>
2016-12-16 00:06:24 +0100

    turns in a server

--- autobot/server.py
+++ autobot/server.py
@@ -2,34 +2,58 @@
 """Imports"""
 from flask import Flask
 from flask import render_template
-import movement
+from movement import Movement as Move
+from turn import Turn
 
 APP = Flask(__name__)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
 
 @APP.route("/")
-def hello():
+def index():
     """ the index route """
     return render_template('index.html')
 
 
-@APP.route("/forward/<int:waittime>/<int:number>")
-def walker(waittime, number):
-    """walker method"""
-    return "done", waittime, number
+@APP.route("/forward/<int:seconds>")
+def forward(seconds):
+    """forward method"""
+    move_fwd = Move()
+    move_fwd.forward(seconds)
+    return "done", seconds
 
 
-@APP.route("/status/")
-@APP.route("/status/<int:pin_id>")
-def status(pin_id=None):
-    """ a status"""
-    if not pin_id:
-        return "none"
-    else:
-        return "status pin %s" % (pin_id)
+@APP.route("/backward/<int:seconds>")
+def backward(seconds):
+    """backward method"""
+    move_bwd = Move()
+    move_bwd.backward(seconds)
+    return "done", seconds
+
+
+@APP.route("/left/<int:seconds>")
+def left(seconds):
+    """left method"""
+    turn_lft = Turn()
+    turn_lft.left(seconds)
+    return "done", seconds
+
+
+@APP.route("/right/<int:seconds>")
+def right(seconds):
+    """right method"""
+    turn_rgt = Turn()
+    turn_rgt.right(seconds)
+    return "done", seconds
+
+
+# @APP.route("/status/")
+# @APP.route("/status/<int:pin_id>")
+# def status(pin_id=None):
+#     """ a status"""
+#     if not pin_id:
+#         return "none"
+#     else:
+#         return "status pin %s" % (pin_id)
 
 
 if __name__ == "__main__":
